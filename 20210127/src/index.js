1f101c714e3cba12a2e79868041cc8d1ef43d349
yukai <1079869931@qq.com>
2021-02-06 22:02:55 +0800

    u'p

--- 20210127/src/index.js
+++ 20210127/src/index.js
@@ -1,10 +1,22 @@
-console.log('index');
+// console.log('index');
 
-class Index {
-    constructor() {
-        console.log(`你好`)
-    }
+// class Index {
+//     constructor() {
+//         console.log(`你好`)
+//     }
+// }
+
+// let index = new Index();
+// console.log(index);
+import 'bootstrap';
+// 因为在webpack.config.js里面起了别名，所以现在直接引用bootstrap去表示引用了bootstrap/dist/css/bootstrap.css这个文件
+// import 'bootstrap/dist/css/bootstrap.css';
+let xhr=new XMLHttpRequest();
+
+xhr.open('GET','/api/user',true);
+
+xhr.onload=function(){
+    console.log(xhr.response);
 }
 
-let index = new Index();
-console.log(index);
+xhr.send();
--- 20210127/webpack.config.js
+++ 20210127/webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
-
+const webpack = require('webpack');
 /**
  * 1、cleanWebpackPlugin-》每次打包前可以清理dist文件夹
- * 2、copyWebpackPlugin
- * 3、bannerPlugin (内置的)
+ * 2、copyWebpackPlugin-》拷贝非打包生成的文件，例如文档等
+ * 3、bannerPlugin (内置的)-》打包代码添加版权声明
  */
 
 module.exports = {
@@ -18,6 +18,17 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'production',  // production development
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                // 当后端接口没有'/api'的前缀时候，可以在调用时把'/api'重写成空
+                pathRewrite: {
+                    '/api': ''
+                }
+            }
+        }
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
@@ -27,7 +38,8 @@ module.exports = {
         new CleanWebpackPlugin(),
         new CopyPlugin([
             { from: "doc", to: "doc" }
-        ])
+        ]),
+        new webpack.BannerPlugin('make 2021 by yukai')
     ],
     // 增加映射文件，可以帮助我们调试源代码->源码映射
     // 会单独生成一个sourcemap文件，出错了会标识，帮助我们定位问题
@@ -41,6 +53,16 @@ module.exports = {
         aggregateTimeout: 500, // 防抖
         ignored: /node_modules/ // 不需要监控的文件
     },
+    resolve: {
+        // 这里让查找node_modules只在当前目录下查找
+        modules: [path.resolve('node_modules')],
+        // 别名
+        // alias: {
+        //     bootstrap: 'bootstrap/dist/css/bootstrap.css'
+        // }
+        // 一般查找node_modules文件先去找pageage.json中设定好的main文件，mainFields配置可以改变查找文件顺序
+        mainFields: ['style', 'main']
+    },
     module: {
         rules: [{
             test: /\.js$/,
@@ -50,6 +72,9 @@ module.exports = {
                     presets: ['@babel/preset-env']
                 }
             }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
         }]
     }
 }
