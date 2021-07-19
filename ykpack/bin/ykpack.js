d91c0534be81bf9be49659f0db018f1e617a682c
yukai pc <1079869931@qq.com>
2021-07-19 23:07:40 +0800

    update ykpack

--- ykpack/bin/ykpack.js
+++ ykpack/bin/ykpack.js
@@ -2,11 +2,11 @@
 /**
  * 1. 需要找到当前执行的路径，拿到webpack.config.js
  */
-let path=require('path');
+let path = require('path');
 // 获取config文件
-let config=require(path.resolve(__dirname));
+let config = require(path.resolve('webpack.config.js'));
 
-let Compiler=require('../lib/Compiler');
-let compiler=new Compiler(config);
+let Compiler = require('../lib/Compiler');
+let compiler = new Compiler(config);
 // 标识运行编译
 compiler.run();
--- ykpack/lib/Compiler.js
+++ ykpack/lib/Compiler.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const fs = require('fs');
 
 class Compiler {
     constructor(config) {
         this.config = config;
         // 需要保存入口文件的路径
-        this.entryId;
+        this.entryId;// './src/index.js'
         // 需要保存所有的模块依赖
         this.modules = {};
         this.entry = config.entry;
@@ -21,12 +22,44 @@ class Compiler {
     }
 
     /**
+     * 获取源文件
+     * @param {*} modulePath 
+     */
+    getSource(modulePath) {
+        let content = fs.readFileSync(modulePath, 'utf-8');
+        return content
+    }
+
+    /**
      * 创建模块
      * @param {*} modulePath 模块路径
      * @param {*} isEntry 是否是入口主文件
      */
     buildModule(modulePath, isEntry) {
+        let source = this.getSource(modulePath);
+        // 模块id modulePath = modulePath - this.root
+        let moduleName = './' + path.relative(this.root, modulePath);
 
+        if (isEntry) {
+            this.entryId = moduleName;
+        }
+
+        // 解析需要把source源码进行改造 返回一个依赖列表
+        let { sourceCode, dependencies } = this.parse(source, path.dirname(modulePath));
+        // 把相对路径和模块中的内容对应起来
+        this.modules[moduleName] = sourceCode;
+    }
+
+    /**
+     * 解析源码 主要依靠AST解析语法树
+     * @param {*} source 
+     * @param {*} parentPath 
+     */
+    parse(source, parentPath) {
+        console.log(source, parentPath);
+        return {
+            sourceCode: source
+        }
     }
 
     emitAssets() {
